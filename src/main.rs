22de7650b475978e98e56b1b94df675e289dd8de
tijan <tijanmdR@gmail.com>
2024-05-05 15:17:13 +1000

    input for first and second number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::{self, stdout};
+
 fn main() {
-    println!("Hello, world!");
+    let mut x: String = String::new();
+    let mut y: String = String::new();
+    let mut op: String = String::new();
+    let res: i32;
+
+    println!("Enter the first number: ");
+    io::stdin().read_line(&mut x).expect("Invalid Input");
+
+    let x: i32 = match x.trim().parse() {
+        Ok(num) => num, 
+        Err(_)=> {
+            println!("Invalid first number!");
+            return;
+        }
+    };
+
+    println!("Enter the second number: ");
+    io::stdin().read_line(&mut y).expect("Invalid Input");
+
+    let y: i32 = match y.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid second number!");
+            return;
+        }
+    };
+
 }
